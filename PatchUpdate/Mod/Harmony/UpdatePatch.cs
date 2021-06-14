0499232291bd422169c22dfd8f7aa7963d079cb0
Kpro <44187035+cravens1968@users.noreply.github.com>
2021-06-13 23:31:21 -0700

    Update patch implements "K"/Prefab. Works great. Next step is to refactor customradial.

---
+++ PatchUpdate/Mod/Harmony/UpdatePatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using PatchUpdate;
+
+[HarmonyPatch(typeof(PlayerMoveController))]
+[HarmonyPatch("Update")]
+class Patch
+{
+    public static bool Prefix(PlayerMoveController __instance )
+    {
+        if (__instance.playerInput.Prefab.WasPressed)
+        {
+            // your stuff here
+            KProCustomRadial.KProSetupRadial();
+            return false;
+        }
+        return true;
+
+    }
+}
